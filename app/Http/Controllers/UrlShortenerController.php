8835181babfa6eb36c6227da294177e919d4ca5f
shua24 <jo.daniel.simanjuntak@gmail.com>
2025-12-30 02:17:35 +0700

    Add URL shortening

---
+++ app/Http/Controllers/UrlShortenerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+use App\Http\Requests\GenerateUrlRequest;
+use App\Actions\GenerateCodeAction;
+
+class UrlShortenerController extends Controller
+{
+    public function generateUrl(GenerateUrlRequest $request, GenerateCodeAction $generateCodeAction)
+    {
+        $code = $generateCodeAction->execute();
+        $shortUrl = Cache::driver('redis')->put($code, $request->url, $request->expires_after);
+
+        $result = config('app.url') . '/' . $code;
+        return compact('result');
+    }
+
+    public function redirectToUrl(string $code)
+    {
+        $result = Cache::driver('redis')->get($code);
+        abort_if(!$result, 404);
+
+        return redirect($result);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\UrlShortenerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{code}', [UrlShortenerController::class, 'redirectToUrl']);
