ae52c8c04cfb4f092a0510207b96d790bc8ef5e8
shua24 <jo.daniel.simanjuntak@gmail.com>
2026-01-01 18:54:42 +0700

    Add health checker route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'url-shortener')->name('url.form');
 
 Route::get('/{code}', [UrlShortenerController::class, 'redirectToUrl']);
+Route::get('/health', fn () => response()->json(['status' => 'ok']));
+
